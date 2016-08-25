6d0049d61a7876a15fbe8d4e068230218464c576
Oscar Vicente Perez <wellan_heavy@hotmail.com>
2016-08-25 18:29:02 +0200

    Some changes in regex

--- Textile Port/Textile/RegexUtils.cs
+++ Textile Port/Textile/RegexUtils.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Textile.Textile
@@ -10,25 +11,26 @@ namespace Textile.Textile
     public class MarkRegex
     {
         // CartifDictionary
-        private static readonly CartifDictionary<String, String> patterns = new Dictionary<String, String>();
+        private CartifDictionary<String, Regex> compiledPatterns = new CartifDictionary<String, Regex>(7);
+        public Regex this[String name] => compiledPatterns[name];
 
-        static MarkRegex()
+        public MarkRegex()
         {
+            // TODO Refactor this with the new CartifDictionary
+            CartifDictionary<String, String> p = new CartifDictionary<String, String>()
+            {
+                ["Class"] = "\\([^\\)]+\\)",
+                ["Style"] = "\\{[^\\}]+\\}",
+                ["Lang"] = "\\[[^\\[\\]]+\\]",
+                ["Align"] = "(?:<>|<|>|=)",
+                ["Pad"] = "[\\(\\)]+",
+                ["Blocks"] = "(?:b[qc]|div|notextile|pre|h[1-6]|fn\\d+|p|###)"
+            };
+
+            p.Add("Attributes", $"(?:{p["Class"]}|{p["Style"]}|{p["Lang"]}|{p["Align"]}|{p["Pad"]})*");
+
+            foreach (var pattern in p)
+                compiledPatterns.Add(pattern.Key, new Regex(pattern.Value, RegexOptions.Compiled));
         }
-
-
-        #region Attributes
-
-        private const String ClassPattern = "\\([^\\)]+\\)";
-        private const String StylePattern = "\\{[^\\}]+\\}";
-        private const String LangPattern = "\\[[^\\[\\]]+\\]";
-        private const String AlignPattern = "(?:<>|<|>|=)";
-        private const String PadPattern = "[\\(\\)]+";
-
-        private static readonly String AttributeUnionPattern = $"(?:{ClassPattern}|{StylePattern}|{LangPattern}|{AlignPattern}|{PadPattern})*";
-
-        #endregion
-
-        private const String BlocksPattern = "(?:b[qc]|div|notextile|pre|h[1-6]|fn\\d+|p|###)";
     }
 }
